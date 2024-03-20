55f8b8c90f4d46d783ecf4b0855d088337a8a202
LiPingan <lipingan.dev@outlook.com>
2024-03-20 22:26:59 +0800

    Using GN as the build system.

---
+++ hello_world.cc
@@ -0,0 +1,17 @@
+/**
+ * @file hello_world.cc
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-03-20
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+
+int main() {
+  std::cout << "hello world" << std::endl;
+  return 0;
+}
